f965670477a21765d86884e414eb39196b139211
nienio1 <admin@oskar-piatek.com>
2025-04-10 10:56:53 -0500

    added modifiers to buttons and textfields

--- ContentView.swift
+++ ContentView.swift
@@ -8,19 +8,34 @@ struct ContentView: View {
         
         TextField("Enter Number 1 here", value: $number1, format: .number)
             .multilineTextAlignment(.center)
+            .textFieldStyle(.roundedBorder)
         TextField("Enter Number 2 here", value: $number2, format: .number)
             .multilineTextAlignment(.center)
+            .textFieldStyle(.roundedBorder)
         HStack{
             Button("Add") {
                 answer = number1 + number2
             }
+            .frame(width: 75, height: 30 )
+            .background(.blue)
+                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+                        .foregroundStyle(.black)
             Button("Multiply") {
                 answer = number1 * number2
             }
+            .frame(width: 75, height: 30 )
+            .background(.blue)
+                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+                        .foregroundStyle(.black)
+            Button("Subtract") {
+                answer = number1 - number2
+            }
+            .frame(width: 75, height: 30 )
+            .background(.blue)
+                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+                        .foregroundStyle(.black)
         }
-        Button("Subtract") {
-            answer = number1 - number2
-        }
+        .padding()
         Text("\(answer)")
     }
 }
