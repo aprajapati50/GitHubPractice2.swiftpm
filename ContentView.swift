c57898b4811d41952141f17e977d3a30c8a06891
nienio1 <admin@oskar-piatek.com>
2025-04-10 10:38:38 -0500

    created number 1 textfield

--- ContentView.swift
+++ ContentView.swift
@@ -4,11 +4,6 @@ struct ContentView: View {
     @State var number1: Int = 0
     @State var number2: Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Number 1", value: $number1, format: .number)
     }
 }
